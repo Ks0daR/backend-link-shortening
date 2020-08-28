c93ec97d5ddd68e383b7a4e8e8bb3b4d85a1687a
Ivan Shevchenko <rayserviz@gmail.com>
2020-08-28 18:02:00 +0300

    compate version

--- client/pages/links/[linkId].js
+++ client/pages/links/[linkId].js
@@ -1,18 +1,52 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 
-export default function LinkPage({ todo }) {
-  console.log(todo);
+export default function LinkPage({ todo: serverTodo }) {
+  const [todo, setTodo] = useState(serverTodo);
+
+  const router = useRouter();
+
+  function handleClick() {
+    router.push("/links");
+  }
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${router.query.linkId}`
+      );
+      const data = await response.json();
+      setTodo(data);
+    }
+
+    if (!serverTodo) {
+      load();
+    }
+  }, []);
+
+  if (!todo) {
+    return (
+      <Layout>
+        <h1>Loading...</h1>
+      </Layout>
+    );
+  }
   return (
     <Layout title="Информация о ссылке">
       <h1>LinkPage</h1>
       <h2>{todo.userId}</h2>
       <p>{todo.title}</p>
+      <button onClick={handleClick}>На страницу всех ссылок</button>
     </Layout>
   );
 }
 
 LinkPage.getInitialProps = async (context) => {
+  if (!context.res) {
+    return { todo: null };
+  }
+
   const id = context.query.linkId;
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/todos/${id}`
--- client/pages/links/index.js
+++ client/pages/links/index.js
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Layout from "../../components/Layout";
 
-export default function LinksPage({ todos }) {
+export default function LinksPage({ todos: serverTodos }) {
+  const [todos, setTodos] = useState(serverTodos);
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/"
+      );
+      const data = await response.json();
+      setTodos(data);
+    }
+
+    if (!serverTodos) {
+      load();
+    }
+  }),
+    [];
+
+  if (!todos) {
+    return (
+      <Layout>
+        <h1>Loading...</h1>
+      </Layout>
+    );
+  }
   return (
     <Layout title="Все ссылки">
       <h1>LinksPage</h1>
@@ -20,7 +45,10 @@ export default function LinksPage({ todos }) {
   );
 }
 
-LinksPage.getInitialProps = async () => {
+LinksPage.getInitialProps = async (ctx) => {
+  if (!ctx.res) {
+    return { todos: null };
+  }
   const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
   const todos = await response.json();
 
