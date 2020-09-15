2ca54fc8b08825eda063604a8e5420f2730ef3f9
Ivan Shevchenko <rayserviz@gmail.com>
2020-09-15 20:03:13 +0300

    working on links component

--- client/src/components/Links/Links.js
+++ client/src/components/Links/Links.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { useAuth } from "../../hooks/useAuth";
 import { useHttp } from "../../hooks/useHttp";
+import Loader from "../Loader/Loader";
+import styles from "./Links.module.css";
 
 const Links = () => {
   const [links, setLinks] = useState(null);
 
-  const { request } = useHttp();
+  const { request, loading } = useHttp();
 
-  console.log(links)
+  console.log(links);
 
   const link = "/links/";
   const auth = useAuth(AuthContext);
@@ -29,7 +31,25 @@ const Links = () => {
     }
   }, [auth.jwtToken]);
 
-  return <pre>{JSON.stringify(links)}</pre>;
+  return (
+    <div className={styles.container}>
+      {links ? (
+        links.map((link) => (
+          <div key={link.code} className={styles.card}>
+            <h2>
+              Откуда: <p>{link.from}</p>
+            </h2>
+            <h4>
+              Короткая ссылка: <p>{link.shortLink}</p>
+            </h4>
+            <span>{link.clicks}</span>
+          </div>
+        ))
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
 };
 
 export default Links;
